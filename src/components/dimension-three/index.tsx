3c0165ade06e23c5c9ea9673521210a0c0df3bf2
dev-rathore <rathoredvd3001@gmail.com>
2024-06-29 20:18:56 +0530

    DimensionThree component

---
+++ src/components/dimension-three/index.tsx
@@ -0,0 +1,34 @@
+import { Canvas } from "@react-three/fiber";
+import * as THREE from "three";
+import ThreeDModels from "./3d-models";
+import { useModelContext } from "../../contexts/model-context.provider";
+import Tabs from "./tabs";
+
+const DimensionThree: React.FC = () => {
+  const {
+  } = useModelContext();
+
+  return (
+    <div className="canvas-block">
+      <Canvas
+        dpr={1}
+        gl={{
+          antialias: true,
+          toneMapping: THREE.ACESFilmicToneMapping,
+          outputColorSpace: THREE.SRGBColorSpace,
+        }}
+        camera={{
+          fov: 45,
+          near: 0.5,
+          far: 200,
+          position: [6, 2, 6],
+        }}
+      >
+        <ThreeDModels />
+      </Canvas>
+      <Tabs />
+    </div>
+  );
+};
+
+export default DimensionThree;
