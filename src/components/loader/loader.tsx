a5741d1c0b65073fb112efc75bb4bd6ef7c9fb17
dev-rathore <rathoredvd3001@gmail.com>
2024-06-30 08:49:00 +0530

    fix: loader component

--- src/components/loader/loader.tsx
+++ src/components/loader/loader.tsx
@@ -1,7 +1,7 @@
 const Loader = () => {
   return (
     <div className="fixed top-0 left-0 right-0 bottom-0 z-100 flex items-center justify-center">
-      <div className="animate-spin rounded-full h-16 w-16 border-b-2 border-primary"></div>
+      <div className="animate-spin rounded-full h-12 w-12 border-4 border-solid border-r-transparent border-l-transparent border-primary"></div>
     </div>
   );
 };
